efd0b21f297181927853b2fea349fd793d6d72be
victorkhornkh@gmail.com <victorkhornkh@gmail.com>
2026-04-01 22:22:35 +0700

    Quick commit!!!

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,10 +1,14 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-
-    constructor(private readonly jwtService: JwtService) {}
+  constructor(private readonly jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
@@ -13,15 +17,15 @@ export class AuthGuard implements CanActivate {
     if (!token) throw new UnauthorizedException('No token provided');
 
     try {
-        const tokenPayload = await this.jwtService.verifyAsync(token);
-        await this.jwtService.verifyAsync(token);
-        request.user = {
-            userId: tokenPayload.sub,
-            username: tokenPayload.username,
-        }
-        return true;
+      const tokenPayload = await this.jwtService.verifyAsync(token);
+      await this.jwtService.verifyAsync(token);
+      request.user = {
+        userId: tokenPayload.sub,
+        username: tokenPayload.username,
+      };
+      return true;
     } catch (error) {
-        throw new UnauthorizedException('Invalid token');
+      throw new UnauthorizedException('Invalid token');
     }
   }
 }
