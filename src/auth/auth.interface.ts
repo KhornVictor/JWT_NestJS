dcc1641ebb4efa728a36c959e8e1e718723f3369
victorkhornkh@gmail.com <victorkhornkh@gmail.com>
2026-04-01 22:02:55 +0700

    Start Project Wednesday Gueknai April Fool

--- src/auth/auth.interface.ts
+++ src/auth/auth.interface.ts
@@ -1,15 +1,15 @@
 export type AuthInput = {
-    username: string;
-    password: string;
-}
+  username: string;
+  password: string;
+};
 
- export type SignInData = {
-    userId: number;
-    username: string;
-}
+export type SignInData = {
+  userId: number;
+  username: string;
+};
 
 export type AuthResult = {
-    accessToken: string;
-    userId: number;
-    username: string;
-}
+  accessToken: string;
+  userId: number;
+  username: string;
+};
